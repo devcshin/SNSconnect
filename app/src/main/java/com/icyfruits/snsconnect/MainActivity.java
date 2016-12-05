3f92495521c461706e30664fe5d565f07a23f370
devcshin <dev.c.shin@gmail.com>
2016-12-05 17:46:04 +0900

    add button

--- app/src/main/java/com/icyfruits/snsconnect/MainActivity.java
+++ app/src/main/java/com/icyfruits/snsconnect/MainActivity.java
@@ -3,11 +3,29 @@ package com.icyfruits.snsconnect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
+
+    //keytool -exportcert -alias androiddebugkey -keystore C:\Users\m09-5\.android\debug.keystore  | openssl sha1 -binary | openssl base64
+    //Qce+5cPoBrUhZu5LF5UFADzGUno=
+
+    //https://developers.facebook.com/docs/facebook-login/android/#addbutton
+    //https://developers.facebook.com/quickstarts/377866572558467/?platform=android
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void login(){
+
     }
 }
